ef574f15a8231a5c733f376bb86e49b233f3f87e
Ian-Roanito <ianroanito027@gmail.com>
2026-04-29 22:47:00 -0300

    Ajustes
    Ajustando o top da nav-bar para os valores novos da altura do header

--- scripts/menu-moile.js
+++ scripts/menu-moile.js
@@ -2,15 +2,18 @@ const menuMobile = document.getElementById("menu-mobile");
 const navBar = document.getElementById("nav-bar");
 const iMenu = document.querySelector(".menu-mobile i");
 
-let header = document.querySelector("#header");
-
+// Abre e fecha o menu hamburguer
 menuMobile.addEventListener('click', () => {
     navBar.classList.toggle("show");
     iMenu.classList.toggle("bi-x-lg");
     iMenu.classList.toggle("bi-list");
-
+    
 });
 
+
+let header = document.querySelector("#header");
+
+// Captura o scroll na tela no eixo Y e adiciona a classe active no header.
 document.addEventListener("scroll", ()=> {
 
     let scroll = window.scrollY;
